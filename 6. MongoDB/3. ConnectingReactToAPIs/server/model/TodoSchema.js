1504f79805df99cb767fc4d3d83fae6d9b15d9ef
Aayushmaan Soni <aayushmaan.soni54@gmail.com>
2024-10-03 00:48:56 +0530

    Fixed a issue in connecting backend api with react

--- 6. MongoDB/3. ConnectingReactToAPIs/server/model/TodoSchema.js
+++ 6. MongoDB/3. ConnectingReactToAPIs/server/model/TodoSchema.js
@@ -3,7 +3,7 @@ const { Schema, mongoose } = require("mongoose");
 const todoSchema = new Schema({
   title: { type: String, required: [ true, "Todo Title is necessary!" ] },
   isCompleted: { type: Boolean, required: true },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, required: true }
 });
 
 
